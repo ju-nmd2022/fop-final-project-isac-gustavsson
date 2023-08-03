03db38c4a005d455466584181276abe647516df7
isacgustavsson <117119420+isacgustavsson@users.noreply.github.com>
2023-08-03 17:53:24 +0200

    ,

--- js/main.js
+++ js/main.js
@@ -55,24 +55,24 @@ menuButton.addEventListener("click", () => {
 });
 
 function preload() {
-  grass = loadImage("assets/grass.png");
-  gravel = loadImage("assets/gravel.png");
-  stone = loadImage("assets/stonetile.png");
-  gold = loadImage("assets/goldtile.png");
+  grass = loadImage("/assets/grass.png");
+  gravel = loadImage("/assets/gravel.png");
+  stone = loadImage("/assets/stonetile.png");
+  gold = loadImage("/assets/goldtile.png");
 
-  playerIdle = loadImage("assets/playerIdle.png");
-  playerSpriteRight = loadImage("assets/playerRight.png");
-  playerSpriteLeft = loadImage("assets/playerLeft.png");
-  playerSpriteUp = loadImage("assets/playerUp.png");
-  playerSpriteDown = loadImage("assets/playerDown.png");
-  hitDownSprite = loadImage("assets/hitDown.png");
-  hitRightSprite = loadImage("assets/hitRight.png");
+  playerIdle = loadImage("/assets/playerIdle.png");
+  playerSpriteRight = loadImage("/assets/playerRight.png");
+  playerSpriteLeft = loadImage("/assets/playerLeft.png");
+  playerSpriteUp = loadImage("/assets/playerUp.png");
+  playerSpriteDown = loadImage("/assets/playerDown.png");
+  hitDownSprite = loadImage("/assets/hitDown.png");
+  hitRightSprite = loadImage("/assets/hitRight.png");
   hitLeftSprite = loadImage("assets/hitLeft.png");
 
-  batSheet = loadImage("assets/batSheet.png");
-  batSheetAlert = loadImage("assets/batSheetAlert.png");
+  batSheet = loadImage("/assets/batSheet.png");
+  batSheetAlert = loadImage("/assets/batSheetAlert.png");
 
-  spiderSheet = loadImage("assets/spiderSheet.png");
+  spiderSheet = loadImage("/assets/spiderSheet.png");
 }
 
 function setup() {
